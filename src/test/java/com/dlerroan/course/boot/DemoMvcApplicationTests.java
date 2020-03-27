f13c0dfa04753fafb3861a427007e7d443d6cf87
DenysFerreira <lerroandeco@gmail.com>
2020-03-26 23:09:26 -0300

    Project created

---
+++ src/test/java/com/dlerroan/course/boot/DemoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlerroan.course.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
