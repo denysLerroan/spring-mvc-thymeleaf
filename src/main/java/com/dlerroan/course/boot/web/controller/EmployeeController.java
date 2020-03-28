cb6ecd5d838bdb080693759f1c10456db931305a
DenysFerreira <lerroandeco@gmail.com>
2020-03-27 22:25:22 -0300

    Testing menu links

---
+++ src/main/java/com/dlerroan/course/boot/web/controller/EmployeeController.java
@@ -0,0 +1,21 @@
+package com.dlerroan.course.boot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/funcionarios")
+public class EmployeeController {
+	
+	@GetMapping("/cadastrar")
+	public String insert() {
+		return "/funcionario/cadastro";
+	}
+	
+	@GetMapping("/listar")
+	public String findAll() {
+		return "/funcionario/lista";
+	}
+
+}
