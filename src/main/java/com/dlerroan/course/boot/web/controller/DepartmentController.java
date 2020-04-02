20cd7dbe020d54fc69d336cd9f653e2a1dc29bf7
DenysFerreira <lerroandeco@gmail.com>
2020-04-02 19:02:40 -0300

    Insert departments

--- src/main/java/com/dlerroan/course/boot/web/controller/DepartmentController.java
+++ src/main/java/com/dlerroan/course/boot/web/controller/DepartmentController.java
@@ -1,20 +1,34 @@
 package com.dlerroan.course.boot.web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dlerroan.course.boot.domain.Department;
+import com.dlerroan.course.boot.service.DepartmentService;
 
 @Controller
 @RequestMapping("/departamentos")
 public class DepartmentController {
+	
+	@Autowired
+	private DepartmentService service;
 
 	@GetMapping("/cadastrar")
-	public String insert() {
+	public String insert(Department department) {
 		return "/departamento/cadastro";
 	}
 	
 	@GetMapping("/listar")
 	public String findAll() {
 		return "/departamento/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String save(Department department) {
+		service.save(department);
+		return "redirect:/departamentos/cadastrar";
 	}
 }
