bcc6a534f18b708b5d5bd39304e4606711a880d3
DenysFerreira <lerroandeco@gmail.com>
2020-04-02 20:29:39 -0300

    Editing departments

--- src/main/java/com/dlerroan/course/boot/web/controller/DepartmentController.java
+++ src/main/java/com/dlerroan/course/boot/web/controller/DepartmentController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -28,9 +29,21 @@ public class DepartmentController {
 		return "/departamento/lista";
 	}
 	
+	@GetMapping("/editar/{id}")
+	public String preUpdate(@PathVariable("id") Long id, ModelMap model) {
+		model.addAttribute("department", service.findById(id));
+		return "/departamento/cadastro";
+	}
+	
 	@PostMapping("/salvar")
 	public String save(Department department) {
 		service.save(department);
+		return "redirect:/departamentos/cadastrar";
+	}
+	
+	@PostMapping("/editar")
+	public String update(Department department) {
+		service.update(department);
 		return "redirect:/departamentos/cadastrar";
 	}
 }
