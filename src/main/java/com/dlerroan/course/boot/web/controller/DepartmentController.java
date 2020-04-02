6dd3f00343708978c16adffe26ae851c5290794c
DenysFerreira <lerroandeco@gmail.com>
2020-04-02 19:13:15 -0300

    Listing departments

--- src/main/java/com/dlerroan/course/boot/web/controller/DepartmentController.java
+++ src/main/java/com/dlerroan/course/boot/web/controller/DepartmentController.java
@@ -2,6 +2,7 @@ package com.dlerroan.course.boot.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,8 @@ public class DepartmentController {
 	}
 	
 	@GetMapping("/listar")
-	public String findAll() {
+	public String findAll(ModelMap model) {
+		model.addAttribute("departamentos", service.findAll());
 		return "/departamento/lista";
 	}
 	
