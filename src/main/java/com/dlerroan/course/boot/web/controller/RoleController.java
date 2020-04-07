b7ab067d4a5768db07c9c7daa9e1934ea891f1d0
DenysFerreira <lerroandeco@gmail.com>
2020-04-06 21:05:25 -0300

    Listing roles

--- src/main/java/com/dlerroan/course/boot/web/controller/RoleController.java
+++ src/main/java/com/dlerroan/course/boot/web/controller/RoleController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,7 +32,8 @@ public class RoleController {
 	}
 	
 	@GetMapping("/listar")
-	public String findAll() {
+	public String findAll(ModelMap model) {
+		model.addAttribute("cargos", roleService.findAll());
 		return "/cargo/lista";
 	}
 	
